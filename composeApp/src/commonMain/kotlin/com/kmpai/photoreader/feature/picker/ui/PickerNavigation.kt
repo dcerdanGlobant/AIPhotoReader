6499c46cad07ea4aaaf426f6db345b6f733a5a53
David-Cerdan-Sanchez <x634392@santanderglobaltech.com>
2024-12-04 16:37:57 +0100

    tmp commit

--- composeApp/src/commonMain/kotlin/com/kmpai/photoreader/feature/picker/ui/PickerNavigation.kt
+++ composeApp/src/commonMain/kotlin/com/kmpai/photoreader/feature/picker/ui/PickerNavigation.kt
@@ -1,10 +1,15 @@
 package com.kmpai.photoreader.feature.picker.ui
 
+import aiphotoreader.composeapp.generated.resources.Res
+import aiphotoreader.composeapp.generated.resources.chat_ia
+import aiphotoreader.composeapp.generated.resources.image_description
+import aiphotoreader.composeapp.generated.resources.select_image
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 import com.kmpai.photoreader.core.navigation.ScreenNavigatorConfig
+import com.kmpai.photoreader.core.ui.TopBarConfig
 import com.kmpai.photoreader.feature.picker.ui.screens.home.PickerHomeScreen
 
 val pickerNavConfig = ScreenNavigatorConfig(
@@ -12,15 +17,24 @@ val pickerNavConfig = ScreenNavigatorConfig(
 )
 
 private val homePickerNavConfig = ScreenNavigatorConfig(
-    "homePicker"
+    "homePicker",
+    title = Res.string.select_image
+)
+
+private val chatIANavConfig = ScreenNavigatorConfig(
+    "chatIA",
+    title = Res.string.chat_ia,
+    topBarBackEnabled = true
 )
 
 fun NavGraphBuilder.addPickerNavGraph(
-    navController: NavController
+    navController: NavController,
+    onTopBarConfig: ((topBarConfig: TopBarConfig) -> Unit),
 ) {
     navigation(startDestination = homePickerNavConfig.route, route = pickerNavConfig.route) {
         composable(route = homePickerNavConfig.route) {
-            PickerHomeScreen()
+
+            PickerHomeScreen() { onTopBarConfig.invoke(it)}
         }
     }
 }
--- composeApp/src/commonMain/kotlin/com/kmpai/photoreader/feature/picker/ui/screens/home/PickerHomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/kmpai/photoreader/feature/picker/ui/screens/home/PickerHomeScreen.kt
@@ -1,10 +1,15 @@
 package com.kmpai.photoreader.feature.picker.ui.screens.home
 
+import aiphotoreader.composeapp.generated.resources.Res
+import aiphotoreader.composeapp.generated.resources.chat_ia
+import aiphotoreader.composeapp.generated.resources.image_description
+import aiphotoreader.composeapp.generated.resources.select_image
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import com.kmpai.photoreader.core.ui.TopBarConfig
 import com.kmpai.photoreader.feature.permission.AlertMessageDialog
 import com.kmpai.photoreader.feature.permission.ImageSourceOptionDialog
 import com.kmpai.photoreader.feature.permission.PermissionCallback
@@ -23,7 +28,8 @@ import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun PickerHomeScreen (
-    viewModel: PickerHomeViewModel = koinViewModel<PickerHomeViewModel>()
+    viewModel: PickerHomeViewModel = koinViewModel<PickerHomeViewModel>(),
+    onTopBarConfig: ((topBarConfig: TopBarConfig) -> Unit),
 ) {
     val homeState by viewModel.homeState.collectAsState()
     val coroutineScope = rememberCoroutineScope()
@@ -71,6 +77,7 @@ fun PickerHomeScreen (
 
     when(homeState) {
         PickerHomeState.ImageSourceOptionDialog -> {
+            onTopBarConfig.invoke(TopBarConfig(Res.string.select_image, false))
             ImageSourceOptionDialog(onDismissRequest = {
                 viewModel.showPickImage()
             }, onGalleryRequest = {
@@ -94,8 +101,12 @@ fun PickerHomeScreen (
             }
         }
         PickerHomeState.LaunchSettings -> permissionsManager.launchSettings()
-        PickerHomeState.PickPicture -> PickView() { viewModel.showImageSourceOptionDialog()}
+        PickerHomeState.PickPicture ->  {
+            onTopBarConfig.invoke(TopBarConfig(Res.string.select_image, false))
+            PickView() { viewModel.showImageSourceOptionDialog()}
+        }
         is PickerHomeState.PickedPicture -> {
+            onTopBarConfig.invoke(TopBarConfig(Res.string.image_description, false))
             val pickedState = homeState as PickerHomeState.PickedPicture
             PickerHomeView(
                 picture = pickedState.picture,
@@ -122,6 +133,7 @@ fun PickerHomeScreen (
             }
 
         is PickerHomeState.OpenChat ->  {
+            onTopBarConfig.invoke(TopBarConfig(Res.string.chat_ia, true))
             val pickedState = homeState as PickerHomeState.OpenChat
             ChatView(
                 picture = pickedState.picture,
