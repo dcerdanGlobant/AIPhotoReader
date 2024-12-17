dbf207498e4bb6d89cfa7cc9bb34cfddb5da3ce9
Miguel Ferrer Fornali <miguelferrerfornali1999@gmail.com>
2024-12-17 13:38:05 +0100

    FIX: Fix ios safe area

--- iosApp/iosApp/View/ContentView.swift
+++ iosApp/iosApp/View/ContentView.swift
@@ -23,9 +23,6 @@ struct ComposeView: UIViewControllerRepresentable {
 struct ContentView: View {
     var body: some View {
         ComposeView()
-            .ignoresSafeArea(.keyboard)
+            .ignoresSafeArea(.all)
     }
 }
-
-
-
